e7e878a588149d517c5ff968c974e2b3e203a2ff
Anton Markelov <markelovanton@gmail.com>
2023-10-23 23:44:31 +0600

    concluded winning combinations

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import {useRef, useState} from "react";
 import PlayCard from "./lib/PlayCard";
 import CardDeck from "./lib/CardDeck";
 import Card from "./lib/Card";
+import Combination from "./lib/Combination";
 
 function App() {
 
   const [deck, setDeck] = useState<PlayCard[]>([]);
+  const [win, setWin] =useState('')
   const ref = useRef(deck)
   const dealCards = () => {
     const newDeck = new CardDeck();
@@ -14,6 +16,11 @@ function App() {
     ref.current = newDeck.deck;
 
     setDeck(flop);
+
+    setWin(new Combination(flop).getOutcome());
+    if (win === 'flash') {
+      alert('WIN FLASH')
+    }
   }
 
   if (!deck.length) {
@@ -25,11 +32,14 @@ function App() {
       <div>
         <button onClick={dealCards}>Deal Cards</button>
         <div className= 'card-box'>
-          <Card rank={deck[0].suit} suit={deck[0].rank}/>
-          <Card rank={deck[1].suit} suit={deck[1].rank}/>
-          <Card rank={deck[2].suit} suit={deck[2].rank}/>
-          <Card rank={deck[3].suit} suit={deck[0].rank}/>
-          <Card rank={deck[4].suit} suit={deck[0].rank}/>
+          <Card rank={deck[0].rank} suit={deck[0].suit}/>
+          <Card rank={deck[1].rank} suit={deck[1].suit}/>
+          <Card rank={deck[2].rank} suit={deck[2].suit}/>
+          <Card rank={deck[3].rank} suit={deck[0].suit}/>
+          <Card rank={deck[4].rank} suit={deck[0].suit}/>
+        </div>
+        <div>
+          <h1>{win}</h1>
         </div>
       </div>
 
