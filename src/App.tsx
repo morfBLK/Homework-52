1970494f3831c762099fd345660c8d80a83cb146
Anton Markelov <markelovanton@gmail.com>
2023-10-24 01:26:47 +0600

    made it possible to replace the card. added checks for an empty deck

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Combination from "./lib/Combination";
 function App() {
 
   const [deck, setDeck] = useState<PlayCard[]>([]);
-  const [win, setWin] =useState('')
+  const [win, setWin] = useState('')
   const ref = useRef(deck)
   const dealCards = () => {
     const newDeck = new CardDeck();
@@ -23,20 +23,54 @@ function App() {
     }
   }
 
+  const changeCard = (number: number) => {
+    const random = Math.floor(Math.random() * ref.current.length);
+    const replacedCard = ref.current.splice(random, 1);
+    const flop: PlayCard[] = [...deck];
+    flop.splice(number, 1, replacedCard[0])
+    setDeck(flop);
+
+    setWin(new Combination(flop).getOutcome());
+    if (win === 'flash') {
+      alert('WIN FLASH')
+    }
+  }
+
+  let disabled = false
+
+  if (ref.current.length < 1) {
+    disabled = true
+  }
+
   if (!deck.length) {
     return (
       <button onClick={dealCards}>Deal Cards</button>
     )
-  } else if (deck.length) {
+  } else {
     return (
       <div>
         <button onClick={dealCards}>Deal Cards</button>
-        <div className= 'card-box'>
-          <Card rank={deck[0].rank} suit={deck[0].suit}/>
-          <Card rank={deck[1].rank} suit={deck[1].suit}/>
-          <Card rank={deck[2].rank} suit={deck[2].suit}/>
-          <Card rank={deck[3].rank} suit={deck[0].suit}/>
-          <Card rank={deck[4].rank} suit={deck[0].suit}/>
+        <div className='card-box'>
+          <div>
+            <Card rank={deck[0].rank} suit={deck[0].suit}/>
+            <button disabled={disabled} onClick={() => changeCard(0)}>Replace</button>
+          </div>
+          <div>
+            <Card rank={deck[1].rank} suit={deck[1].suit}/>
+            <button disabled={disabled} onClick={() => changeCard(1)}>Replace</button>
+          </div>
+          <div>
+            <Card rank={deck[2].rank} suit={deck[2].suit}/>
+            <button disabled={disabled} onClick={() => changeCard(2)}>Replace</button>
+          </div>
+          <div>
+            <Card rank={deck[3].rank} suit={deck[3].suit}/>
+            <button disabled={disabled} onClick={() => changeCard(3)}>Replace</button>
+          </div>
+          <div>
+            <Card rank={deck[4].rank} suit={deck[4].suit}/>
+            <button disabled={disabled} onClick={() => changeCard(4)}>Replace</button>
+          </div>
         </div>
         <div>
           <h1>{win}</h1>
